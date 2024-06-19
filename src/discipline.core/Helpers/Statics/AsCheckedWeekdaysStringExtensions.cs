f928dd2979c7eb33c134bf5dfd5e0980f277ad47
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-06-19 21:39:24 +0200

    Added components for deleting activity rule. Improved components for activity rules list

--- src/discipline.core/Helpers/Statics/AsCheckedWeekdaysStringExtensions.cs
+++ src/discipline.core/Helpers/Statics/AsCheckedWeekdaysStringExtensions.cs
@@ -4,9 +4,9 @@ namespace discipline.core.Helpers.Statics;
 
 public static class AsCheckedWeekdaysStringExtensions
 {
-    public static string AsCheckedWeekdaysString(this ActivityRuleDto dto)
+    public static string AsCheckedWeekdaysString(this ActivityRuleDto dto, string separator = null)
         => dto?.Weekdays is null || dto?.Weekdays.Count == 0
             ? string.Empty
-            : string.Join(", ", dto?.Weekdays?.Where(x
-                => x.IsChecked).Select(x => x.Name)!);
+            : $"{separator ?? string.Empty} {string.Join(", ", dto?.Weekdays?.Where(x
+                => x.IsChecked).Select(x => x.Name)!)}";
 }
