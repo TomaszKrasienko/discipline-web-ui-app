7b0f978961a20a49497bf9c244e5c52d551ad3f3
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-06-23 19:09:51 +0200

    Added components for browse and add activity

--- src/discipline.core/Dispatchers/Models/DailyProductivity/ActivityRequest.cs
+++ src/discipline.core/Dispatchers/Models/DailyProductivity/ActivityRequest.cs
@@ -1,3 +1,6 @@
 namespace discipline.core.Dispatchers.Models.DailyProductivity;
 
-public sealed class ActivityRequest(string Title);
+public sealed class ActivityRequest
+{
+    public string Title { get; set; }
+}
