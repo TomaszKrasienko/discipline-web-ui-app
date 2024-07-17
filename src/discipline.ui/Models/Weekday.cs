d7bd9d9bd8241e07674d1afaeebcdb61e083f9d6
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-07-17 20:20:49 +0200

    Finished form as a template for User calendar

--- src/discipline.ui/Models/Weekday.cs
+++
@@ -1,8 +0,0 @@
-namespace discipline.ui.Models;
-
-public class Weekday
-{
-    public string Name { get; set; } = string.Empty;
-    public bool IsChecked { get; set; }
-    public int Id { get; set; }
-}
