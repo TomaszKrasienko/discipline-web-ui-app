a5315f8ce1bd0995af9a1f6d6a73690ae6dcce92
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-08-25 12:52:53 +0200

    Creating wasm

--- wasm/discipline-wasm-ui/discipline-wasm-ui/Program.cs
+++ wasm/discipline-wasm-ui/discipline-wasm-ui/Program.cs
@@ -5,6 +5,7 @@ using discipline_wasm_ui;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddBootstrapBlazor();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
