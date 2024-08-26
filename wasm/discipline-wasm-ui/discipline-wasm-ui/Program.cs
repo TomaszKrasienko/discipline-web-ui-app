dfa5a6dcfbcc0d53d01cb5c5420e40ecc1c1e6b1
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-08-26 21:42:38 +0200

    Added handling sign-in with adding tokens to local storage. Temporary without cryptography

--- wasm/discipline-wasm-ui/discipline-wasm-ui/Program.cs
+++ wasm/discipline-wasm-ui/discipline-wasm-ui/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using discipline_wasm_ui;
+using discipline_wasm_ui.Configuration;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddBootstrapBlazor();
+builder.Services.AddConfiguration();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
--- wasm/discipline-wasm-ui/discipline-wasm-ui/Services/Client/Internals/DisciplineClient.cs
+++ wasm/discipline-wasm-ui/discipline-wasm-ui/Services/Client/Internals/DisciplineClient.cs
@@ -1,31 +1,48 @@
+using System.Net.Http.Json;
 using discipline_wasm_ui.Services.Client.Abstractions;
 
 namespace discipline_wasm_ui.Services.Client.Internals;
 
-internal sealed class DisciplineClient : IDisciplineClient
+internal sealed class DisciplineClient(
+    HttpClient httpClient) : IDisciplineClient
 {
-    public Task<HttpResponseMessage> GetAsync(string path)
+    public async Task<HttpResponseMessage> GetAsync(string path)
     {
-        throw new NotImplementedException();
+        await Authorize();
+        return await httpClient.GetAsync(path);
     }
 
-    public Task<HttpResponseMessage> PostAsync<T>(string path, T t) where T : class
+    public async Task<HttpResponseMessage> PostAsync<T>(string path, T t) where T : class
     {
-        throw new NotImplementedException();
+        await Authorize();
+        return await httpClient.PostAsJsonAsync<T>(path, t);
     }
 
-    public Task<HttpResponseMessage> PutAsync<T>(string path, T t) where T : class
-    {
-        throw new NotImplementedException();
+    public async Task<HttpResponseMessage> PutAsync<T>(string path, T t) where T : class
+    { 
+        await Authorize();
+        return await httpClient.PutAsJsonAsync<T>(path, t);
     }
 
-    public Task<HttpResponseMessage> PatchAsync(string path)
-    {
-        throw new NotImplementedException();
+    public async Task<HttpResponseMessage> PatchAsync(string path)
+    { 
+        await Authorize();
+        return await httpClient.PatchAsync(path, null);
     }
 
-    public Task<HttpResponseMessage> DeleteAsync(string path)
+    public async Task<HttpResponseMessage> DeleteAsync(string path)
     {
-        throw new NotImplementedException();
+        await Authorize();
+        return await httpClient.DeleteAsync(path);
+    }
+
+    private Task Authorize()
+    {
+        return Task.CompletedTask;
+        // var tokens = await tokenStorage.Get();
+        // if (tokens is not null)
+        // {
+        //     httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", tokens.Token);
+        // }
     }
 }
