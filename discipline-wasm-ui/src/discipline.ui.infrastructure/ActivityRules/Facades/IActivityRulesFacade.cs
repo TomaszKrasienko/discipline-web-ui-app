bd725459dd69606dfaf8f9309166da6aaebff857
Tomasz Krasienko <tomasz.krasienko@outlook.com>
2025-04-06 23:20:31 +0200

    creating activity rules

--- discipline-wasm-ui/src/discipline.ui.infrastructure/ActivityRules/Facades/IActivityRulesFacade.cs
+++ discipline-wasm-ui/src/discipline.ui.infrastructure/ActivityRules/Facades/IActivityRulesFacade.cs
@@ -3,28 +3,31 @@ using System.Net.Http.Json;
 using discipline.ui.communication.http.ActivityRules;
 using discipline.ui.communication.http.ActivityRules.DTOs.Responses;
 using Microsoft.AspNetCore.Components;
+using OneOf;
 
 namespace discipline.ui.infrastructure.ActivityRules.Facades;
 
 public interface IActivityRulesBrowseFacade
 {
-    Task<IReadOnlyList<ActivityRuleResponseDto>> HandleAsync(CancellationToken cancellationToken);
+    Task<OneOf<IReadOnlyList<ActivityRuleResponseDto>, string>> HandleAsync(CancellationToken cancellationToken);
 }
 
 internal sealed class ActivityRulesBrowseFacade(
     IActivityRulesHttpService activityRulesHttpService,
     NavigationManager navigationManager) : IActivityRulesBrowseFacade
 {
-    public async Task<IReadOnlyList<ActivityRuleResponseDto>> HandleAsync(CancellationToken cancellationToken)
+    public async Task<OneOf<IReadOnlyList<ActivityRuleResponseDto>, string>> HandleAsync(CancellationToken cancellationToken)
     {
         var response = await activityRulesHttpService.GetActivityRules(cancellationToken);
         
         if (response.StatusCode == HttpStatusCode.Unauthorized)
         {
-            navigationManager.NavigateTo("/sign-in");
+            return "user.unauthorized";
         }
         
-        return await response.Content.ReadFromJsonAsync<IReadOnlyList<ActivityRuleResponseDto>>(cancellationToken)
+        var result = await response.Content.ReadFromJsonAsync<IReadOnlyList<ActivityRuleResponseDto>>(cancellationToken)
             ?? [];
+        
+        return OneOf<IReadOnlyList<ActivityRuleResponseDto>, string>.FromT0(result);
     }
 }
